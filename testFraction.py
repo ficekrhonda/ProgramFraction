4f7d0d8091280535e3fdbcca64271deb0d98cea4
R Ficek <ficek@users.noreply.github.com>
2016-04-22 15:29:32 -0500

    Create testFraction.py
    test constructor

---
+++ testFraction.py
@@ -0,0 +1,6 @@
+from modFraction import *
+f1 = Fraction(1,2); print("f1 1/2 : ",f1)
+f2 = Fraction(4,6); print("f2 2/3 : ",f2)
+f3 = Fraction(-5/10); print("-1/2: ",f3)
+f4 = Fraction(6/-18); print("-1/3: ", f4)
+f5 = Fraction( );  print("testing defaults: ",f5)
